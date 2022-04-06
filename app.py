f38ce96acf18240d46c4946877bc9b6302ef8c7d
krishanharwani <krishanharwani1@gmail.com>
2022-04-06 15:35:50 +0530

    #2

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "App is Running"
-    
+    return "App is Running..."
+
 @app.route('/updateCSV/')
 def index():
     data = request.args.get('data')
