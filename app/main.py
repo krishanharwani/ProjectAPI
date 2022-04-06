714f236d29169225bc2c249ba7ec4e666564a1be
krishanharwani <krishanharwani1@gmail.com>
2022-04-06 19:28:40 +0530

    added getcsv function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-from distutils.log import debug
-from flask import Flask, request
+from flask import Flask, request, make_response
+import pandas as pd
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+
+try:
+    file = open('data.csv', 'a')
+    file.write("PH, TURBUDITY\n")
+    file.close()
+except Exception as err:
+    print(err)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,7 +26,17 @@ def updateCSV():
         x, y = map(float, data.split(","))
         file.write(f"{x}, {y}\n")
         print("Data Updated in CSV")
+        file.close()
         return "0"
     except Exception as err:
         print(err)
         return "-1"
+
+@app.route('/getCSV/', methods=['GET', 'POST'])
+def getCsv():
+    df = pd.read_csv(f"{os.getcwd()}\\data.csv")
+    resp = make_response(df.to_csv(index=False))
+    resp.headers["Content-Disposition"] = "attachments; filename=data.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return resp
+    # return "csd"
